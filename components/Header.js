c8465d16517f8c4e27acdfae6cabc5932c40fadf
Shawn Martinez <52Martinezs66@gmail.com>
2020-06-17 20:39:08 -0500

    save changes

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,5 @@ export default () => `
     <p>
       These beautiful trails and parks in St. Louis County
     </p>
-    <ul>
-      <li><a href="beeTreePark.html">Bee Tree Park</a></li>
-      <li><a href="cliffCavePark.html">Cliff Cave Park</a></li>
-      <li><a href="jbPark.html">JB Park</a></li>
-      <li><a href="arnoldPark.html">Arnold Park</a></li>
-    </ul>
-     </header>
+</header>
 `;
--- store/Home.js
+++ store/Home.js
@@ -1,4 +1,4 @@
 export default {
   header: "My Home Page",
-  veiw: "Home"
+  view: "Home"
 };
