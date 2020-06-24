eee35a682504d9fec83ed10b6e0695f9eed337e0
Shawn Martinez <52Martinezs66@gmail.com>
2020-06-24 14:21:25 -0500

    save updates

--- components/views/Gallery.js
+++ components/views/Gallery.js
@@ -1,5 +1,5 @@
 const memory = require("./../../assets/BeeTreeHoursSign.jpg");
-const pywebsite = require("./../../assets/BeeTreeHoursSign.jpg");
+const pywebsite = require("./../../assets/BeeTreeLakeSign.jpg");
 const reactSW = require("./../../assets/BeeTreeDeck.jpg");
 
 export default st => `
@@ -14,15 +14,15 @@ export default st => `
 </div>
 <br />
 <div class="container row justify-content-center">
-    <h1 class="row justify-content-center animated fadeIn">Py Entertainment</h1>
-  <p class="animated fadeIn">This is a basic Python 2.7 script that takes dictionaries and then renders a website that then displays those dictionaries in a card like format that includes a modal that plays the movies trailer. Clicking the image takes you to the Github repo.</p>
+    <h1 class="row justify-content-center animated fadeIn">Bee Tree Lake</h1>
+  <p class="animated fadeIn">This is some information on Bee Tree Lake.</p>
   <div class="imgContainer">
     <a href="https://github.com/BunchDevelopment/movie_website" target="_blank"><img src="${pywebsite}" class="blur portfolioIMG" id="pyImg"></a>
   </div>
 <br />
 <div class="container row justify-content-center">
-    <h1 class="row justify-content-center animated fadeIn">React Star Wars</h1>
-  <p class="animated fadeIn">A small little website that is built with React, that pulls in data from an API of Star Wars characters, and then displays that data! It also uses an animation library. The Details are hidden and revealed using State! </p>
+    <h1 class="row justify-content-center animated fadeIn">Bee Tree Deck</h1>
+  <p class="animated fadeIn">This is a deck on the other side of Bee Tree Park from the lake overlooking the Mississippi River.</p>
   <div class="imgContainer">
     <a href="https://github.com/BunchDevelopment/Starwars_React_API" target="_blank"><img src="${reactSW}" class="blur portfolioIMG" id="memoryImg"></a>
     <br />
