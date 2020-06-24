58b952d6c01b2b28c040271d170a045706bddccf
Shawn Martinez <52Martinezs66@gmail.com>
2020-06-24 14:56:58 -0500

    save update

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,3 +1,3 @@
-export default st => `
-<p id="home">${st.paragraph}</p>
+export default () => `
+<section id="home"><p>This is an informational website about a few of the parks and bike trails here in <b>St.louis County</b>! These parks include <b>Bee Tree Park</b> and <b>Cliff Cave Park</b>.</p></section>
 `;
