6dcf0760a529bc3d56b5f3cdbf199d77fdef75ce
Shawn Martinez <52Martinezs66@gmail.com>
2020-06-24 17:33:31 -0500

    last update HS

--- store/Info.js
+++ store/Info.js
@@ -2,5 +2,6 @@ export default {
   header: "Bee Tree Info",
   page: "Info",
   paragraph:
-    "Bee Tree Park is at 2701 Finestown Ave, St. Louis, Mo.This is one of the most under rated parks in St.Louis County that offers a little of everything. Many couples have married here because it is such a beautiful park. Bee Tree Park offers many things to to do from fish, hiking, biking, to playgrounds for the kids."
+    "Bee Tree Park is at 2701 Finestown Ave, St. Louis, Mo. This is one of the most under rated parks in St.Louis County that offers a little of everything. Many couples have married here because it is such a beautiful park. Bee Tree Park offers many things to to do from fish, hiking, biking, to playgrounds for the kids."
+    "Cliff Cave Park is at 806 Cliff Cave Rd, St. Louis, Mo. Here is another beautiful natural resource in St. Louis County that also offers a scenic bike trail that sprawls roughly 2.5 miles. Like the name states there are also caves at this park."
 };
