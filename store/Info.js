4c6803725a5c1e86a1e5083b5183eeae60cfb197
Shawn Martinez <52Martinezs66@gmail.com>
2020-06-24 15:01:41 -0500

    save update

--- store/Info.js
+++ store/Info.js
@@ -2,5 +2,5 @@ export default {
   header: "Bee Tree Info",
   page: "Info",
   paragraph:
-    "2701 Finestown Ave, St. Louis, Mo.This is one of the most under rated parks in St.Louis County that offers a little of everything. Many couples have married here because it is such a beautiful park. Bee Tree Park offers many things to to do from fish, hiking, biking, to playgrounds for the kids."
+    "Bee Tree Park is at 2701 Finestown Ave, St. Louis, Mo.This is one of the most under rated parks in St.Louis County that offers a little of everything. Many couples have married here because it is such a beautiful park. Bee Tree Park offers many things to to do from fish, hiking, biking, to playgrounds for the kids."
 };
